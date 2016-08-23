bd870bd68ec6027e473aff06744f4ce85303b06f
selin <selin@techart.ru>
2016-08-23 16:20:28 +0300

    Script url for github.io

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by selin on 03.08.16.
  */
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(function(registration) {
+	navigator.serviceWorker.register('/push/sw.js').then(function(registration) {
 		console.log('ServiceWorker registration successful with scope: ',    registration.scope);
 	}).catch(function(err) {
 		console.log('ServiceWorker registration failed: ', err);
@@ -15,7 +15,6 @@ function showNotification() {
 			navigator.serviceWorker.ready.then(function(registration) {
 				registration.showNotification('Vibration Sample', {
 					body: 'Buzz! Buzz!',
-					icon: '../images/touch/chrome-touch-icon-192x192.png',
 					vibrate: [200, 100, 200, 100, 200, 100, 200],
 					tag: 'vibration-sample'
 				});
